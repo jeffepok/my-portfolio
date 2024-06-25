f97f9666fc03ea509b75a687bd6f126c6788da85
jeffersonaddai-poku <jeffepok@gmail.com>
2024-06-25 23:50:54 +0000

    add project title

--- src/components/ui/cards/slide.tsx
+++ src/components/ui/cards/slide.tsx
@@ -9,6 +9,9 @@ export default function Slide({ project }: { project: Project }) {
                 <img className="w-full h-full object-cover" src={project.imageUrl || ""} alt="Card Image" />
             </div>
             <div className="p-4 flex-1 flex flex-col">
+                <div className="">
+                    {project.title}
+                </div>
                 <div className="px-6 pt-4 pb-2">
                     {
                         project.stack?.map((stack, index) => (<span key={`stack_` + index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{stack}</span>
